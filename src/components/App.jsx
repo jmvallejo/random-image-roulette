061c096db3306881838207410a394d484673beb0
Juan Manuel Vallejo <jmvallejo@gmail.com>
2020-05-26 12:05:55 -0500

    Add start screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Roulette from './Roulette'
 import './App.scss'
 
-const App = () => <Roulette />
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showRoulette: false
+    }
+  }
+
+  start () {
+    this.setState({
+      showRoulette: true
+    })
+  }
+
+  render () {
+    const { showRoulette } = this.state
+    if (showRoulette) {
+      return <Roulette />
+    } else {
+      return (
+        <div className='start-screen'>
+          <div className='click-element' onClick={this.start.bind(this)}>
+            Click to start roulette
+          </div>
+        </div>
+      )
+    }
+  }
+}
 
 export default App
